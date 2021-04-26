899d1aefb440747c5f1a9075595488d9eb411208
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-04-26 18:36:42 +0700

    reset (2)

--- ASPNETCore/ASPNETCore/Context/myContext.cs
+++ ASPNETCore/ASPNETCore/Context/myContext.cs
@@ -10,17 +10,6 @@ namespace ASPNETCore.Context
     public class MyContext : DbContext
     {
         public MyContext() : base("myConnection") { }
-        public DbSet<Address> Addresses { get; set; }
-        public DbSet<Contact> Contacts { get; set; }
-        public DbSet<District> Districts { get; set; }
-        public DbSet<Education> Educations { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<EmployeeRole> EmployeeRoles { get; set; }
-        public DbSet<Major> Majors { get; set; }
-        public DbSet<Province> Provinces { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<SubDistrict> SubDistricts { get; set; }
-        public DbSet<Territory> Territories { get; set; }
-        public DbSet<University> Universities { get; set; }
+       
     }
 }
