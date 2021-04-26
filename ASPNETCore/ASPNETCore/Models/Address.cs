329915f9b15d64df2a51630bcc6c9707984ef1d3
Alfahmist <48145002+alfahmist@users.noreply.github.com>
2021-04-26 10:45:33 +0700

    Model Address

---
+++ ASPNETCore/ASPNETCore/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore.Controllers.Models
+{
+    [Table("TB_M_Address")]
+    public class Address
+    {   
+        [Key]
+        public int Id { get; set; }
+        public string StreetAddress1 { get; set; }
+        public string StreetAddress2 { get; set; }
+
+        [ForeignKey("Territory")]
+        public Territory Territory { get; set; }
+    }
+}
