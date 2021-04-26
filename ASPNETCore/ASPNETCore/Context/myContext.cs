650cad5e79abc52ff193d44766eda695b7ea8216
khanzarg <khanzarahmasilla@gmail.com>
2021-04-26 21:09:06 +0700

    update

--- ASPNETCore/ASPNETCore/Context/myContext.cs
+++ ASPNETCore/ASPNETCore/Context/myContext.cs
@@ -9,12 +9,25 @@ namespace ASPNETCore.Context
 {
     public class MyContext : DbContext
     {
-        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        public MyContext(DbContextOptions<MyContext> options)
+            : base(options)
         {
 
         }
 
-        public DbSet<EmployeeRole> EmployeeRoles { get; set; }
+        public DbSet<Employee> Employees { get; set; }
         public DbSet<Role> Roles { get; set; }
+        public DbSet<EmployeeRole> EmployeeRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeRole>()
+                .HasOne(EmployeeRole => EmployeeRole.Employee)
+                .WithMany(Employee => Employee.EmployeeRoles);
+
+            modelBuilder.Entity<EmployeeRole>()
+                .HasOne(EmployeeRole => EmployeeRole.Role)
+                .WithMany(Role => Role.EmployeeRoles);
+        }
     }
 }
