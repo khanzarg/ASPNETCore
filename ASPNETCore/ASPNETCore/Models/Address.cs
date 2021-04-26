34001b8e48881d1b6330767d88e8bc5ab8b151a5
khanzarg <khanzarahmasilla@gmail.com>
2021-04-26 14:07:02 +0700

    update fahmi

--- ASPNETCore/ASPNETCore/Models/Address.cs
+++ ASPNETCore/ASPNETCore/Models/Address.cs
@@ -14,8 +14,8 @@ namespace ASPNETCore.Models
         public int Id { get; set; }
         public string StreetAddress1 { get; set; }
         public string StreetAddress2 { get; set; }
-        [ForeignKey("Territory")]
         public int Territory_id { get; set; }
+        [ForeignKey("Territory_id")]
         public Territory Territory { get; set; }
       
     }
