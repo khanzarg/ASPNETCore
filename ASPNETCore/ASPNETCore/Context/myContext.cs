5f539f8fde90eb60f1bd14fa7ac730f32b6941dd
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-04-26 14:07:31 +0700

    update myCtx

--- ASPNETCore/ASPNETCore/Context/myContext.cs
+++ ASPNETCore/ASPNETCore/Context/myContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ASPNETCore.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,18 @@ namespace ASPNETCore.Context
 {
     public class myContext : DbContext
     {
+        public myContext() : base("myConnection") { }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeRole> EmployeeRoles { get; set; }
+        public DbSet<Major> Majors { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<SubDistrict> SubDistricts { get; set; }
+        public DbSet<Territory> Territories { get; set; }
+        public DbSet<University> Universities { get; set; }
     }
 }
