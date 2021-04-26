06e0ec45c001bff888ff11be0e63071517cbe63c
Alfatehan Arsya Baharin <alfatehanarsya8998@gmail.com>
2021-04-26 20:50:48 +0700

    Tambah Employee dan MyContext

--- ASPNETCore/ASPNETCore/Context/myContext.cs
+++ ASPNETCore/ASPNETCore/Context/myContext.cs
@@ -1,11 +1,50 @@
-﻿using System;
+﻿using ASPNETCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASPNETCore.Context
 {
-    public class myContext
+    public class MyContext : DbContext
     {
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+                
+        }
+
+       public DbSet<Employee> Employees { get; set; }
+       public DbSet<Contact> Contacts { get; set; }
+       public DbSet<Address> Addresses { get; set; }
+       public DbSet<Education> Educations { get; set; }
+       public DbSet<EmployeeRole> EmployeeRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>()
+                .HasOne(employee => employee.Contact)
+                .WithOne(contact => contact.Employee)
+                .HasForeignKey<Contact>(contact => contact.Id);
+            
+            modelBuilder.Entity<Employee>()
+                .HasOne(employee => employee.Address)
+                .WithOne(address => address.Employee)
+                .HasForeignKey<Address>(address => address.Id);
+            
+            modelBuilder.Entity<Employee>()
+                .HasOne(employee => employee.Education)
+                .WithOne(education => education.Employee)
+                .HasForeignKey<Education>(education => education.Id);
+
+            //modelBuilder.Entity<Employee>()
+            //    .HasMany(company => company.EmployeeRoles)
+            //    .WithOne(employeeroles => employeeroles.Employee);
+                
+
+
+            
+        }
     }
 }
