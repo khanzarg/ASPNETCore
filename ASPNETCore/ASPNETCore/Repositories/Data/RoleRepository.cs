56a37ab1d2af9541dfd5478b7c3a870395acf433
Khanzarg <khanzarahmasilla@gmail.com>
2021-04-27 15:19:50 +0700

    update RoleRepository

---
+++ ASPNETCore/ASPNETCore/Repositories/Data/RoleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore.Repositories.Data
+{
+    public class RoleRepository
+    {
+    }
+}
