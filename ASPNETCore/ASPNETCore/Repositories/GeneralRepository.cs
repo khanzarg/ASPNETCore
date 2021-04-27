ff5268ec4755ac67b9e63594a58b0d54301c9475
Ilham Prasetyo <Ilprasetyo2125@gmail.com>
2021-04-27 15:12:14 +0700

    Update Repository and Controller EmployeeRole

---
+++ ASPNETCore/ASPNETCore/Repositories/GeneralRepository.cs
@@ -0,0 +1,46 @@
+﻿using ASPNETCore.Context;
+using ASPNETCore.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore.Repositories
+{
+    public class GeneralRepository<T> : IGenericRepository<T> where T : class
+    {
+        private MyContext _context = null;
+        private DbSet<T> table = null;
+
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Post(T obj)
+        {
+            table.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Put(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
--- ASPNETCore/ASPNETCore/Repositories/Interface/IGenericRepository.cs
+++ ASPNETCore/ASPNETCore/Repositories/Interface/IGenericRepository.cs
@@ -9,9 +9,9 @@ namespace ASPNETCore.Repositories.Interface
     {
         IEnumerable<T> GetAll();
         T GetById(object id);
-        void Insert(T obj);
-        void Update(T obj);
+        void Post(T obj);
+        void Put(T obj);
         void Delete(object id);
-        void Save();
+        
     }
 }
