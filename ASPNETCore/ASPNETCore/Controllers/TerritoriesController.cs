7a23ed0621b0eb614d21a8017f1f16ba9e4dc02a
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-04-27 15:05:22 +0700

    Update TerritoriesController.cs

--- ASPNETCore/ASPNETCore/Controllers/TerritoriesController.cs
+++ ASPNETCore/ASPNETCore/Controllers/TerritoriesController.cs
@@ -10,14 +10,14 @@ using System.Threading.Tasks;
 
 namespace ASPNETCore.Controllers
 {
-    [Route("api/{controller}")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TerritoriesController : ControllerBase
     {
         TerritoryRepository territoryRepository = new TerritoryRepository();
-        private readonly Territory territory = new Territory();
+        
         [HttpGet]
-        [Route("api/{controller}")]
+        //[Route("api/{controller}")]
         public ActionResult Get() 
         {
             var model = territoryRepository.GetTerritories();
@@ -25,7 +25,7 @@ namespace ASPNETCore.Controllers
         }
 
         [HttpGet]
-        [Route("api/{controller}/id")]
+        //[Route("api/{controller}/id")]
         public ActionResult Get(int id) 
         {
             Territory model = territoryRepository.GetById(id);
@@ -33,23 +33,25 @@ namespace ASPNETCore.Controllers
         }
 
         [HttpPost]
-        [Route("api/{controller}")]
+        //[Route("api/{controller}")]
         public ActionResult Post() 
         {
+            Territory territory = new Territory();
             territoryRepository.AddTerritory(territory);
             return Ok("Data has been successfully inserted.");
         }
         [HttpDelete]
-        [Route("api/{controller}/id")]
+        //[Route("api/{controller}/id")]
         public ActionResult Delete(int id) 
         {
             territoryRepository.DeleteTerritory(id);
             return Ok("Data has been successfully deleted.");
         }
         [HttpPut]
-        [Route("api/{controller}/id")]
+        //[Route("api/{controller}/id")]
         public ActionResult Put(int id) 
         {
+            Territory territory = new Territory();
             territoryRepository.GetById(id);
             territoryRepository.UpdateTerritory(territory);
             return Ok("Data has been successfully updated.");
