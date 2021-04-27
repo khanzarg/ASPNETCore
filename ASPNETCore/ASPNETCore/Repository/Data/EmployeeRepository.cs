ac09c597128b05936f725bf327cfa6b96321d9f5
Alfatehan Arsya Baharin <alfatehanarsya8998@gmail.com>
2021-04-27 16:10:57 +0700

    Generic Repository

---
+++ ASPNETCore/ASPNETCore/Repository/Data/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using ASPNETCore.Models;
+using ASPNETCore.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore.Repository.Data
+{
+    public class EmployeeRepository : GeneralRepository<Employee>
+    {
+        private IGenericRepository<Employee> repository = null;
+        public EmployeeRepository()
+        {
+            repository = new GeneralRepository<Employee>();
+        }
+        //public EmployeeRepository(IGenericRepository<Employee> repository)
+        //{
+        //    this.repository = repository;
+        //}
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            var get = repository.GetAll();
+            return get;
+        }
+        public Employee GetEmployeeById(int id)
+        {
+            var getId = repository.GetById(id);
+            return getId;
+        }
+        public void InsertEmployee(Employee employee)
+        {
+            repository.Insert(employee);
+            repository.Save();
+        }
+        public void UpdateEmployee(Employee employee)
+        {
+            repository.Update(employee);
+            repository.Save();
+        }
+        public void DeleteEmployee(int id)
+        {
+            repository.Delete(id);
+            repository.Save();
+        }
+    }
+}
---
+++ ASPNETCore/ASPNETCore/Repository/GeneralRepository.cs
@@ -0,0 +1,50 @@
+﻿using ASPNETCore.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore.Repository.Interface
+{
+    public class GeneralRepository<T> : IGenericRepository<T> where T : class
+    {
+        private MyContext _context = null;
+        private DbSet<T> table = null;
+        
+
+        public void Delete(object id)
+        {
+            T entityToDelete = table.Find(id);
+            //Delete(entityToDelete);
+
+            table.Remove(entityToDelete);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            table.Attach(obj);
+            _context.Entry(obj).State = EntityState.Modified;
+        }
+    }
+}
