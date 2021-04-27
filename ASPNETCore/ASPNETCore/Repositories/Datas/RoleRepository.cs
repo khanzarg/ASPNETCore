6424a077e6b63a0a94eeae4f5aa58a9e1fd8fd60
Khanzarg <khanzarahmasilla@gmail.com>
2021-04-27 14:53:04 +0700

    Update

--- ASPNETCore/ASPNETCore/Repositories/Datas/RoleRepository.cs
+++ ASPNETCore/ASPNETCore/Repositories/Datas/RoleRepository.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using ASPNETCore.Context;
+using ASPNETCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ASPNETCore.Repositories.Datas
 {
-    public class RoleRepository : GeneralRepository
+    public class RoleRepository : GeneralRepository<Role>
     {
 
     }
