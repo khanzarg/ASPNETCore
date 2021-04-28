67791b6f8e71e3b2cdc0356e566419687a9f1b6a
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-04-29 05:23:55 +0700

    Done creating Filter

---
+++ ASPNETCore/ASPNETCore/Filter/BasicAuthenticationAttribute.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace ASPNETCore.Filter
+{
+    public class BasicAuthenticationAttribute : AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization != null)
+            {
+                var authToken = actionContext.Request.Headers.Authorization.Parameter;
+
+                // decoding authToken we get decode value in 'Username:Password' format  
+                var decodeauthToken = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(authToken));
+
+                // spliting decodeauthToken using ':'   
+                var arrUserNameandPassword = decodeauthToken.Split(':');
+
+                var userName = arrUserNameandPassword[0];
+                var password = arrUserNameandPassword[1];
+
+                // Replace this with your own system of security / means of validating credentials
+                var isValid = userName == "sa" && password == "sapassword";
+
+                // at 0th postion of array we get username and at 1st we get password  
+                if (isValid)
+                {
+                    // setting current principle  
+                    var principal = new GenericPrincipal(new GenericIdentity(userName), null);
+                    Thread.CurrentPrincipal = principal;
+
+                    actionContext.Response =
+                       actionContext.Request.CreateResponse(HttpStatusCode.OK,
+                          "User " + userName + " successfully authenticated");
+
+                    return;
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                    actionContext.Response.Headers.Add("WWW-Authenticate", "Basic Scheme='Data' location = 'http://localhost:");
+                }
+            }
+            else
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                actionContext.Response.Headers.Add("WWW-Authenticate", "Basic Scheme='Data' location = 'http://localhost:");
+            }
+        }
+    }
+}
