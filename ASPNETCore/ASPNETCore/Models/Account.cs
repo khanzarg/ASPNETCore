10157e5d98290381b352253fdd37ae5d9a0c8f82
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-05-03 09:53:51 +0700

    Create Account.cs

---
+++ ASPNETCore/ASPNETCore/Models/Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore.Models
+{
+    [Table("TB_M_Account")]
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
