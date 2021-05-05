acc92aa740a22f8a3d01636b57ca0d8e41a2a47e
alfifutuhi13 <alfi.futuhi13@gmail.com>
2021-05-05 10:24:02 +0700

    Create Hash.cs

---
+++ ASPNETCore/ASPNETCore/Handlers/Hash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCore.Handlers
+{
+    public class Hash
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool ValidatePassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
